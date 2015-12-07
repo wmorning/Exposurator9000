6e37f96b5de2089adfc3f75f2490ea8ee6e270d0
Joe <joe.derose13@gmail.com>
2015-12-06 18:30:09 -0800

    Minor changes to ConvNNet

--- ConvNNet.py
+++ ConvNNet.py
@@ -1,7 +1,7 @@
 import numpy as np
 import inDianajonES as InD
 import tensorflow as tf
-
+import sys
 
 '''
 ConvNNet implements a convolutional neural network 
@@ -33,18 +33,21 @@ class ConvNNet(object):
     using the TensorFlow framework.
     '''
     def __init__(self, nimg, farts, gridsize, cgfactor, mbsize=100,
-                 mbpath='/home/jderose/scratch/des/data', batchsize=20):
+                 mbpath='/home/jderose/scratch/des/data', batchsize=1,
+                 cgafactor=1):
         
         self.nimg = nimg
         self.farts = farts
         self.gridsize = gridsize
         self.cgfactor = cgfactor
+        self.cgafactor = cgafactor
         self.Nmb = (self.nimg+mbsize-1)//mbsize
         self.Ncategories = 29
         self.mbpath = mbpath
         self.mbsize = mbsize
         self.batchsize = batchsize
-        self.Nstepspermb = 10
+        self.Nstepspermb = 20
+        self.savefreq = 1000
 
 
     def convert_labels(self, y):
@@ -55,10 +58,10 @@ class ConvNNet(object):
         for i in range(len(ey)):
             ey2[i,ey[i]-1] = 1.0
             
-        return ey
+        return ey2
 
     
-    def load_minibatch(self, filepath, nimg, farts, gridsize, cg, num):
+    def load_minibatch(self, filepath, nimg, farts, gridsize, cg, num,cg_additional=1):
         """
         Load a mini batch of images and their labels. 
         Labels need to be converted to tensorflow
@@ -71,17 +74,21 @@ class ConvNNet(object):
         gridsize -- Number of pixels to a side
         cg -- Coarsegraining factor 
         num -- The minibatch number 
+        cg_additional -- additional coursegraining to perform on the fly
         """
-        
+            
         X = np.load('{0}/X_{1}_{2}_{3}_{4}_mb{5}.npy'.format(filepath, nimg, farts, gridsize, cg, num))
         y = np.load('{0}/y_{1}_{2}_{3}_{4}_mb{5}.npy'.format(filepath, nimg, farts, gridsize, cg, num))
-
+        X[X==-99] = 0
+        if cg_additional!=1:
+            X = np.mean(np.mean(X.reshape([X.shape[0],gridsize//cg,gridsize//cg//cg_additional,cg_additional]),axis=3).T.reshape(gridsize//cg//cg_additional,gridsize//cg//cg_additional,cg_additional,X.shape[0]),axis=2).T.reshape([X.shape[0],(gridsize//cg//cg_additional)**2])
+        X -= np.mean(X, axis=0)
         ey = self.convert_labels(y)
-        
+
         return X, ey
     
     def Train(self, Nsteps, Nfeatures_conv1=32, Wsize_1=5, Nfeatures_conv2=64, \
-                Wsize_2=5, Xlen_3=1024):
+                Wsize_2=5, Xlen_3=1024, gpu=False):
         '''
         This function creates the design matrix and loads the
         true clasifications (if they don't already exist).  
@@ -115,8 +122,8 @@ class ConvNNet(object):
         # start neural net: define x,y placeholders and create session
         #self.Session = tf.InteractiveSession()  # useful if running from notebook
         print('Allocating placeholders')
-        self.x = tf.placeholder("float",shape=[None,self.gridsize**2])
-        self.x_image = tf.reshape(self.x,[-1,self.gridsize,self.gridsize,1])    
+        self.x = tf.placeholder("float",shape=[None,(self.gridsize//(self.cgfactor*self.cgafactor))**2])
+        self.x_image = tf.reshape(self.x,[-1,(self.gridsize//(self.cgfactor*self.cgafactor)),(self.gridsize//(self.cgfactor*self.cgafactor)),1])    
         self.y_ = tf.placeholder("float",shape=[None,self.Ncategories])
     
         # create first layer
@@ -150,11 +157,11 @@ class ConvNNet(object):
         # Densely Connected layer
         # Here, the 7x7x64 image tensor is flattened, and we get a 
         # 1x1024 vector using the form h_fc1 = h_2 * W + b
-        self.W_fc1 = weight_variable([(self.gridsize//self.cgfactor//4)**2*Nfeatures_conv2, Xlen_3])
+        self.W_fc1 = weight_variable([(self.gridsize//(self.cgfactor*self.cgafactor)//4)**2*Nfeatures_conv2, Xlen_3])
         self.b_fc1 = bias_variable([Xlen_3])
         self.h_pool2_flat = tf.reshape(self.h_pool2, [-1, \
-                                  (self.gridsize//self.cgfactor//4) \
-                                  *(self.gridsize//self.cgfactor//4)*Nfeatures_conv2])
+                                  (self.gridsize//(self.cgfactor*self.cgafactor)//4) \
+                                  *(self.gridsize//(self.cgfactor*self.cgafactor)//4)*Nfeatures_conv2])
         self.h_fc1 = tf.nn.relu(tf.matmul(self.h_pool2_flat, self.W_fc1)+self.b_fc1)
     
         print('Dropout')
@@ -176,8 +183,10 @@ class ConvNNet(object):
         
         print('Setting optimization parameters')
         # run the optimization.  We'll minimize the cross entropy
-        self.cross_entropy = -tf.reduce_sum(self.y_*tf.log(self.y_conv))
-        self.train_step = tf.train.AdamOptimizer(1e-4).minimize(self.cross_entropy)
+        #self.cross_entropy = -tf.reduce_sum(self.y_*tf.log(tf.clip_by_value(self.y_conv,1e-10,1.0)))
+        #self.train_step = tf.train.AdamOptimizer(1e-15, epsilon=0.1).minimize(self.cross_entropy)
+        self.chisq = tf.reduce_mean(tf.pow(tf.sub(self.y_,self.y_conv),2)+1e-4)
+        self.train_step = tf.train.GradientDescentOptimizer(1e-4).minimize(self.chisq)
         self.correct_prediction = tf.equal(tf.argmax(self.y_conv,1), tf.argmax(self.y_,1))
         self.accuracy = tf.reduce_mean(tf.cast(self.correct_prediction,"float"))
 
@@ -186,27 +195,41 @@ class ConvNNet(object):
         
         # batch gradient descent ticker
         current_index = 0
-        for i in range(self.Nmb):
-            print('Minibatch {0}'.format(i))
-            self.X, self.y = self.load_minibatch(self.mbpath, self.nimg, self.farts, self.gridsize,
-                                       self.cgfactor, i)
-            for j in range(self.Nstepspermb):
-                print('Batch {0}'.format(j))
-                # update the parameters using batch gradient descent.
-                # use 50 examples per iteration (can change)
-                next_set = np.arange(current_index,current_index+self.batchsize,1)% self.mbsize
-                x_examples = self.X[next_set,:]
-                y_examples = self.y[next_set,:]
-                current_index = (current_index+self.batchsize) % self.mbsize
-                
-                #for every thousandth step, print the training error.
-                if (i*self.Nstepspermb+j)%1000 ==0:
-                    train_accuracy = self.accuracy.eval(feed_dict={self.x:x_examples \
-                                                       , self.y_: y_examples, self.keep_prob: 1.0},session=self.Session)
-                    print "step %d, training accuracy %g"%(i, train_accuracy)
-        
-                self.train_step.run(feed_dict={self.x: x_examples, self.y_: y_examples, self.keep_prob: 0.5},session=self.Session)
+        testX, testy = self.load_minibatch(self.mbpath, self.nimg, self.farts, self.gridsize,
+                                           self.cgfactor, 18, cg_additional=self.cgafactor)
 
+        for npass in range(Nsteps):
+            for i in range(self.Nmb):
+                print('Minibatch {0}'.format(i))
+                self.X, self.y = self.load_minibatch(self.mbpath, self.nimg, self.farts, self.gridsize,
+                                                     self.cgfactor, i, cg_additional=self.cgafactor)
+                for j in range(self.Nstepspermb):
+                    print('Batch {0}'.format(j))
+                    # update the parameters using batch gradient descent.
+                    # use 50 examples per iteration (can change)
+                    next_set = np.arange(current_index,current_index+self.batchsize,1)% self.mbsize
+                    x_examples = self.X[next_set,:]
+                    y_examples = self.y[next_set,:]
+                    current_index = (current_index+self.batchsize) % self.mbsize
+                    
+                #for every thousandth step, print the training error.
+                    if (i*self.Nstepspermb+j)%10 ==0:
+                        train_accuracy = self.accuracy.eval(feed_dict={self.x:x_examples \
+                                                                       , self.y_: y_examples, self.keep_prob: 1.0},session=self.Session)
+                        print "step %d, training accuracy %g"%(i, train_accuracy)
+                        
+                    self.train_step.run(feed_dict={self.x: x_examples, self.y_: y_examples, self.keep_prob: 0.5},session=self.Session)
+                    #print( 'Loss value: {0}'.format(loss_value))
+                    #print( 'W_conv1: {0}'.format(self.W_conv1.eval(session=self.Session) ))
+                    #print( 'b_conv1: {0}'.format(self.b_conv1.eval(session=self.Session) ))
+                    #print( 'y_conv: {0}'.format(self.y_conv.eval(feed_dict={self.x:x_examples, self.y_:y_examples, self.keep_prob: 1.0}, session=self.Session) ))
+                    if (i%self.savefreq==0) & (i!=0):
+                        if gpu:
+                            self.Save_model('Trained_Model_{0}_{1}_{2}_{3}_gpu_mb{4}.tfm'.format(self.nimg, self.farts, self.gridsize,  (self.cgfactor*self.cgafactor), i), i*self.Nstepspermb)
+                        else:
+                            self.Save_model('Trained_Model_{0}_{1}_{2}_{3}_mb{4}.tfm'.format(self.nimg, self.farts, self.gridsize, (self.cgfactor*self.cgafactor), i), i*self.Nstepspermb)
+            
+            self.Test(testX, testy)
 
         return
     
@@ -253,14 +276,14 @@ def weight_variable(shape):
     '''
     Initialize a tensorflow weight variable
     '''
-    initial = tf.truncated_normal(shape, stddev=0.1)
+    initial = tf.random_normal(shape, stddev=0.1)
     return tf.Variable(initial) # note: this won't let us spread across multiple GPUs.
 
 def bias_variable(shape):
     '''
     Initialize a tensorflow bias variable
     '''
-    initial = tf.constant(0.1,shape=shape)
+    initial = tf.random_normal(shape, stddev=0.1)
     return tf.Variable(initial)
 
 def conv2d(x,W):
@@ -276,3 +299,13 @@ def max_pool_2x2(x):
     return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
 
 # ------------------------------------------------------------
+
+if __name__=='__main__':
+
+    if len(sys.argv)>1:
+        gpu = True
+    else:
+        gpu = False
+
+    cnn = ConvNNet(1000, 1.0, 2048, 2, cgafactor=8)
+    cnn.Train(100, Nfeatures_conv1=16, Nfeatures_conv2=32, Xlen_3=10, gpu=gpu)
